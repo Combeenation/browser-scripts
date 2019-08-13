10bccddf7a7ab4b48e9912ca55293516db2653f4
Michael Harrer <michael.harrer@combeenation.com>
2019-08-13 15:42:48 +0200

    Show dev-env-mark on cfgrs

--- Cbn/cbn-dev.user.js
+++ Cbn/cbn-dev.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         CbnDevUsers
 // @namespace    http://www.combeenation.com
-// @version      0.7.1
+// @version      0.7.2
 // @description  Combeenation dev users script
 // @author       Michael Harrer
 // @match        https://portal.combeenation.com/*
@@ -19,5 +19,23 @@ const intervalId = window.setInterval(function() {
       Cbn.utils.Log.printLogs = true;
     }
     Cbn.utils.Log._printSigSloLogs = false;
+    
+    if (Cbn.Cfgr && Cbn.utils.EnvHelper.inDevEnv && !window.location.href.includes('cbn-no-devenv-mark=')) {
+      const devEnvMarkHtml = '<div class="devenv-mark"></div>';
+      const logoUrl = 'https://raw.githubusercontent.com/Combeenation/browser-scripts/master/Cbn/devenv-mark.png';
+      const devEnvMarkStyles = {
+        position:   'absolute',
+        bottom:     '8px',
+        right:      '8px',
+        width:      '64px',
+        height:     '64px',
+        background: `url(${ logoUrl }) no-repeat center / contain`,
+        'z-index':  999999,
+      };
+      
+      window.$(devEnvMarkHtml)
+        .appendTo('body')
+        .css(devEnvMarkStyles);
+    }
   }
 }, 100);
