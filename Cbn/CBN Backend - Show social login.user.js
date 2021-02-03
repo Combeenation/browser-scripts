c778e31704d48541c7b320f7fc3812c0b93b2dd1
FAL <tobias.falkner@combeenation.com>
2021-02-03 18:13:06 +0100

    adjusted css-styling of 'devenv-mark' and match new dev environments via regex

--- Cbn/CBN Backend - Show social login.user.js
+++ Cbn/CBN Backend - Show social login.user.js
@@ -5,12 +5,7 @@
 // @description  Show the deprecated social login
 // @author       Tobias Falkner
 // @match        https://portal.combeenation.com/*
-// @match        https://dev.combeenation.com/*
-// @match        https://dev1.combeenation.com/*
-// @match        https://dev2.combeenation.com/*
-// @match        https://dev3.combeenation.com/*
-// @match        https://dev4.combeenation.com/*
-// @match        https://dev5.combeenation.com/*
+// @include      /^https:[/][/]dev\d*[.]combeenation[.]com[/].*/
 // @match        https://devcore.combeenation.com/*
 // @match        https://127.0.0.1:446/*
 // @grant        none
--- Cbn/cbn-dev.user.js
+++ Cbn/cbn-dev.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         CbnDevUsers
 // @namespace    http://www.combeenation.com
-// @version      0.7.3
+// @version      0.8.0
 // @description  Combeenation dev users script
 // @author       Michael Harrer
 // @match        https://portal.combeenation.com/*
-// @match        https://dev.combeenation.com/*
 // @match        https://127.0.0.1:446/*
+// @include      /^https:[/][/]dev\d*[.]combeenation[.]com[/].*/
 // @grant        none
 // ==/UserScript==
 
@@ -28,10 +28,12 @@ const intervalId = window.setInterval(function() {
         position:   'absolute',
         bottom:     '8px',
         right:      '8px',
-        width:      '64px',
-        height:     '64px',
+        width:      '50px',
+        height:     '50px',
         background: `url(${ logoUrl }) no-repeat center / contain`,
+        opacity: 0.9,
         'z-index':  999999,
+        'pointer-events': 'none'
       };
       
       window.$(devEnvMarkHtml)
