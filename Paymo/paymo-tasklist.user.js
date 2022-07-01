3f1cfe251b54b84fc19905339d5c341634aaa1b7
ENZ <martin.enzelsberger@combeenation.com>
2022-07-02 01:27:26 +0200

    Fix Paymo TaskList IDs script to check repeatedly
    The script must run repeatedly, since TaskLists are loaded delayed and
    user can navigate between projects.

--- Paymo/paymo-tasklist.user.js
+++ Paymo/paymo-tasklist.user.js
@@ -8,11 +8,23 @@
 // @grant        none
 // ==/UserScript==
 
+const IdNodeMarkerClass = "cbnTaskListVisualizer";
+
 (function () {
   'use strict';
-  var taskListNodes = document.querySelectorAll('.tasks__groups-header-title--tasklist');
-  taskListNodes.forEach(function (taskListNode) {
-    let taskListId = taskListNode.attributes['record-id'].value;
-    taskListNode.nextSibling.insertAdjacentHTML('afterEnd', `<span class="tasks__group-items-count"> — ${taskListId}</span>`);
-  });
+  window.setInterval(() => {
+    var taskListNodes = document.querySelectorAll('.tasks__groups-header-title--tasklist');
+    taskListNodes.forEach(function (taskListNode) {
+      // check if ID was already inserted
+      let potentialIdNode = taskListNode.nextSibling.nextSibling;
+      let potentialIdNodeClass = potentialIdNode && potentialIdNode.attributes['class'];
+      if (potentialIdNodeClass && potentialIdNodeClass.value && potentialIdNodeClass.value.indexOf(IdNodeMarkerClass) > 0) {
+        return;
+      }
+
+      // insert TaskList ID
+      let taskListId = taskListNode.attributes['record-id'].value;
+      taskListNode.nextSibling.insertAdjacentHTML('afterEnd', `<span class="tasks__group-items-count ${IdNodeMarkerClass}"> — ${taskListId}</span>`);
+    });
+  }, 2000);
 })();
